c294c3177e97361e773953f37f250d39d8a9adef
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-12-08 09:53:46 -0500

    Se muestra los todos en pantalla.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,8 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     todos: [
-      { description: '#1', completed: false },
-      { description: '#2', completed: false }
+      { id: 1, description: '#1', completed: false },
+      { id: 2, description: '#2', completed: false }
     ]
   },
   getters: {
@@ -14,9 +14,24 @@ export default createStore({
     },
     pendingTodos(state, getters, rootState) {
       return state.todos.filter(t => !t.completed)
+    },
+    getTodosByStatus: (_, getters, __) => (status) => {
+      switch (status) {
+        case 'completed':
+          return getters.completedTodos
+        case 'pending':
+          return getters.pendingTodos
+        default:
+          return getters.allTodos
+      }
     }
   },
   mutations: {
+    toggleTodoStatus(state, id) {
+      const index = state.todos.findIndex(t => t.id === id)
+      const todo = state.todos[index]
+      todo.completed = !todo.completed
+    }
   },
   actions: {
   },
--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,23 +1,65 @@
 <template>
     <h2>Lista de tareas</h2>
-    {{ pending }}
+    <h4>Pendientes: {{ pending.length }}</h4>
+
+    <hr>
+
+    <button @click="currentTab = 'all'" :class="{ 'active': currentTab === 'all' }">Todos</button>
+    <button @click="currentTab = 'pending'" :class="{ 'active': currentTab === 'pending' }">Pendientes</button>
+    <button @click="currentTab = 'completed'" :class="{ 'active': currentTab === 'completed' }">Completados</button>
+
+    <div>
+        <ul>
+            <li v-for="todo of todos" :class="{ 'completed': todo.completed }" :key="todo.id"
+                @dblclick="onToggleTodo(todo.id)">
+                {{ todo.description }}
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
-import { computed } from 'vue'
+import { ref, computed } from 'vue'
 import { useStore } from 'vuex'
 
 export default {
     setup() {
         const store = useStore()
 
+        const currentTab = ref('all')
+
         return {
-            pending: computed(() => store.getters['pendingTodos'])
+            currentTab,
+            pending: computed(() => store.getters['pendingTodos']),
+            todos: computed(() => store.getters['getTodosByStatus'](currentTab.value)),
+            onToggleTodo: (id) => store.commit('toggleTodoStatus', id)
         }
     }
 }
 </script>
 
 <style scoped>
+div {
+    display: flex;
+    justify-content: center;
+    text-align: center;
+}
 
+ul {
+    width: 300px;
+    text-align: center;
+}
+
+li {
+    cursor: pointer;
+}
+
+.active {
+    color: white;
+    background-color: #2c3e50;
+}
+
+.completed {
+    text-decoration: line-through;
+}
 </style>
